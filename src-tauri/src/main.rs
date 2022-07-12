0faa0c420ef1bb18c252e5bd41a4d30017e1ab8b
Ian Hansen <221826+supershabam@users.noreply.github.com>
2022-07-12 14:39:27 -0600

    toggle sleep

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,52 @@
     windows_subsystem = "windows"
 )]
 
+use nosleep::{NoSleep, NoSleepType};
+use std::sync::{Arc, Mutex};
+use tauri::State;
+
+use tauri::{Manager, SystemTray, SystemTrayEvent};
+
+pub struct NoSleepState {
+    enable: bool,
+    handle: NoSleep,
+}
+
+impl Default for NoSleepState {
+    fn default() -> Self {
+        NoSleepState {
+            enable: false,
+            handle: NoSleep::new().unwrap(),
+        }
+    }
+}
+
 fn main() {
+    let state = Arc::new(Mutex::new(NoSleepState::default()));
+
+    let tray = SystemTray::new();
     tauri::Builder::default()
-        .plugin(tauri_plugin_nosleep::init())
+        .system_tray(tray)
+        .manage(state)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } => {
+                let state: State<'_, Arc<Mutex<NoSleepState>>> = app.try_state().unwrap();
+                let mut no_sleep = state.lock().unwrap();
+                no_sleep.enable = !no_sleep.enable;
+                if no_sleep.enable {
+                    println!("sleeping enabled");
+                    no_sleep.handle.stop().unwrap();
+                } else {
+                    println!("sleeping disabled");
+                    no_sleep
+                    .handle
+                    .start(NoSleepType::PreventUserIdleDisplaySleep)
+                    .unwrap();
+                }
+            }
+            _ => {}
+        })
+        // .plugin(tauri_plugin_nosleep::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
